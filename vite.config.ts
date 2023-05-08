f0003f1da899d6c09f70de5148289d0e2f0a2479
chenxingzhi <chen.xingzhi@beicaizs.com>
2023-05-08 10:55:54 +0800

    chore: package config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'index'
+      name: 'Vue3Iframe',
+      fileName: 'vue3-iframe'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
